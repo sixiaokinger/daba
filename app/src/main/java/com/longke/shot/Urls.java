81878671f8e5f418be760cb69efe3f6a6c504f12
yanbo <1360695995@qq.com>
2018-03-12 11:52:19 +0800

    Initial commit

---
+++ app/src/main/java/com/longke/shot/Urls.java
@@ -0,0 +1,24 @@
+package com.longke.shot;
+
+/**
+ * Created by longke on 2017/12/13.
+ */
+
+public class Urls {
+    public static  String BASE_URL="";
+
+    public static final String GetTrainStudentData = BASE_URL +"/api/TrainStudent/GetTrainStudentData";
+    public static final String DeviceIsRegist = BASE_URL +"/api/Device/DeviceIsRegist";
+    public static final String GetConfigData = BASE_URL +"/api/Device/GetConfigData";
+    public static final String EndShoot = BASE_URL +"/api/TrainStudent/EndShoot";
+    public static final String StartShoot = BASE_URL +"/api/TrainStudent/StartShoot";
+    public static final String ChangeMode = BASE_URL +"/api/TrainStudent/ChangeMode";
+    public static final String GuestRealBeginShoot = BASE_URL +"/api/TrainStudent/GuestRealBeginShoot";
+    public static final String GetUseTime = BASE_URL +"/api/TrainStudent/GetUseTime";
+    public static final String RegistPad = BASE_URL +"/api/Device/RegistPad";
+    public static final String GetTrainStudentDataByGroupId = BASE_URL +"/api/TrainStudent/GetTrainStudentDataByGroupId";
+
+
+
+
+}
